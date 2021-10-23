2115cc4d66dc87e5d40d92926950d2ce14908212
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-23 17:36:13 +0530

    Welcome to Stock account management Program

---
+++ src/management/stockaccount/StockAccountManagement.java
@@ -0,0 +1,11 @@
+package management.stockaccount;
+
+public class StockAccountManagement {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to stock account management program");
+
+	}
+
+}
